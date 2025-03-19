1ad26410ab30971459abcabbac0ffaa27ee8e675
jennymakki <jjennyeklundd@hotmail.com>
2025-03-19 21:33:04 +0100

    Added comments to the test files.

--- feedback.test.js
+++ feedback.test.js
@@ -1,17 +1,22 @@
 import { it, describe, expect } from "@jest/globals";
 import feedback from "./feedback.js";
 
+// Test for the function 'Feedback'.
 describe("feedback()", () => {
+
+  // Making sure there is a 'correct' word that the other word can be compared to. 
   it("has a correct word", () => {
     const input = feedback("");
     expect(input).toEqual("cykla");
   });
 
+  // This test ensures that there can be an input of a guessed word.
   it("takes a guessed word", () => {
     const input = feedback("hallå");
     expect(input).toEqual(expect.any(Array));
   });
 
+  // This test will compare the guessed word with the correct word and give information about each letter. 
   it("returns an array with objects for each letter in the guessed word and the result for the letter", () => {
     const output = feedback("hallå");
     expect(output).toEqual([
--- wordChoice.test.js
+++ wordChoice.test.js
@@ -1,19 +1,23 @@
 import { it, describe, expect } from '@jest/globals';
 import wordChoice from './wordChoice';
 
+// Test for the function 'wordChoice'.
 describe("wordChoice()", () => {
     const words = ["cykla", "solen", "våg", "hund", "äpple", "tåg", "lampa", "robot"];
 
+    // This test will ensure a random word from the array is selected.
     it("returns a random word from the list", () => {
         const word = wordChoice();
         expect(words).toContain(word);
     });
 
+    // This test will test that a word with the specific length (5), is selected. 
     it("returns a word with the specified length", () => {
         const word = wordChoice(5);
         expect(word.length).toBe(5);
     });
 
+    // Makes it possible to not play the game with words with duplicate letters.
     it("returns a word without duplicate letters when allowDuplicates is false", () => {
         const word = wordChoice(null, false);
         if (word) {
@@ -22,6 +26,7 @@ describe("wordChoice()", () => {
         }
     });
 
+    // If there is no word that match the criteria, in this case a word that is longer than ten symbols, 'null' will be returned. 
     it("returns null if no words match the criteria", () => {
         const word = wordChoice(10);
         expect(word).toBeNull();
